26c2021aeab4647062b4d1e5216de9512153010d
Pedro Espinola <pedrohgespinola@live.com>
2021-09-10 13:06:28 -0300

    att js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ let botao = document.querySelector("#enviar")
 let data  = document.querySelector("#data")
 
 botao.addEventListener("click", function(){
-    console.log("funcionando")
     let dataEscolhida = data.value
     requisicao(dataEscolhida)
-    console.log("a data foi " + dataEscolhida)
-
 })
 
 function requisicao(dataEscolhida){
@@ -17,11 +14,12 @@ function requisicao(dataEscolhida){
 
     let request = new XMLHttpRequest
 
-    request.open("GET", `https://api.nasa.gov/planetary/apod?api_key=gb8N4ADtCncjCg2CPLeJY1nqhQZ0Tch7jbP4T1Yy&date=${dataEscolhida}`,false)
+    request.open("GET", `https://api.nasa.gov/planetary/apod?api_key=gb8N4ADtCncjCg2CPLeJY1nqhQZ0Tch7jbP4T1Yy&date=${dataEscolhida}`)
+    request.send()
 
     request.addEventListener("load", (event)=>{
     if (request.status==200){
-        let dados = JSON.parse(request.responseText)
+        let dados = JSON.parse(request.response)
         titulo.innerText = dados.title
         data.innerText=dados.date
         descricao.innerText = dados.explanation
@@ -30,3 +28,30 @@ function requisicao(dataEscolhida){
     }
 })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.button').on('click', async function (event) {
+//     event.preventDefault();
+//     $('#main-texto').css("display", "none");
+//     let inputDate = $('#dia').val();
+//     const url = `https://api.nasa.gov/planetary/apod?api_key=gb8N4ADtCncjCg2CPLeJY1nqhQZ0Tch7jbP4T1Yy&date=${inputDate}`
+//     let apiResponse = await fetch(url).then(response => response.json());
+//     $('body').css("background-image", `url(${apiResponse.url})`);
+//     $('body').css("background-repeat", "no-repeat");
+//     $('body').css("background-size", "cover");
+//     $('#texto-nasa').css('display', 'block');
+//     $('#nasa-titulo').html(`${apiResponse.title}`);
+//     $('#nasa-texto').html(`(${apiResponse.explanation})`);
+// });
